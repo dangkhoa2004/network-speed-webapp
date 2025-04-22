6c40eb30a808124c048e1f56dcf4f1dab2a62c91
chin <149858139+dangkhoa2004@users.noreply.github.com>
2025-04-22 23:22:37 +0700

    [UPDATE LIBRARY]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import time
 import socket
 import psutil
@@ -14,18 +14,27 @@ app.static_folder = 'static'
 def home():
     return render_template("index.html")
 
-from flask import request
+def get_network_info():
+    info = []
+    for interface, addrs in psutil.net_if_addrs().items():
+        for addr in addrs:
+            if addr.family == socket.AF_INET:  # IPv4
+                info.append({
+                    "interface": interface,
+                    "ip": addr.address,
+                    "netmask": addr.netmask,
+                    "broadcast": addr.broadcast,
+                })
+    return info
+
+for i in get_network_info():
+    print(i)
 
 @app.route("/api/publicip")
 def get_public_ip_info():
     try:
-        # IP mặc định từ ip-api (dành cho local)
         ip_api_url = "http://ip-api.com/json/"
-
-        # Kiểm tra IP truy cập
         client_ip = request.headers.get("X-Forwarded-For", request.remote_addr).split(",")[0].strip()
-
-        # Nếu không phải local (localhost hoặc IP nội bộ) thì dùng IP thật
         if not (
             client_ip.startswith("127.") or
             client_ip.startswith("192.168.") or
@@ -69,34 +78,50 @@ def speed_test():
         })
     except Exception as e:
         return jsonify({"error": f"Lỗi không xác định: {str(e)}"}), 500
-
 @app.route("/api/networkinfo")
 def network_info():
     try:
-        info = {}
+        info = {
+            "interfaces": []
+        }
+
         for name, addrs in psutil.net_if_addrs().items():
+            iface = {
+                "interface": name,
+                "ipv4": None,
+                "ipv6": [],
+                "netmask": None
+            }
+
             for addr in addrs:
+                # IPv4
                 if addr.family == socket.AF_INET:
-                    info["IPv4"] = addr.address
-                    info["Subnet Mask"] = addr.netmask
+                    ip = addr.address
+                    if ip.startswith(("127.", "169.", "26.")):  # loại IP loopback, APIPA, VPN
+                        continue
+                    iface["ipv4"] = ip
+                    iface["netmask"] = addr.netmask
+
+                # IPv6
                 elif addr.family == socket.AF_INET6:
-                    if addr.address.startswith("fe80"):
-                        info["Link-local IPv6"] = addr.address
-                    elif "temporary" in str(addr):
-                        info["Temporary IPv6"] = addr.address
-                    else:
-                        info["IPv6"] = addr.address
+                    ipv6 = addr.address
+                    if ipv6.startswith("fe80"):  # link-local IPv6
+                        continue
+                    iface["ipv6"].append(ipv6)
 
-        interfaces = list(psutil.net_if_stats().keys())
-        if interfaces:
-            stats = psutil.net_if_addrs().get(interfaces[0], [])
-            for addr in stats:
-                if addr.family == socket.AF_INET:
-                    info["Default Gateway"] = ".".join(addr.address.split(".")[:-1] + ["1"])
-                    break
+            # Chỉ thêm nếu có địa chỉ hợp lệ
+            if iface["ipv4"] or iface["ipv6"]:
+                info["interfaces"].append(iface)
 
-        dns_servers = []
-        if os.name != 'nt':  # Linux/macOS
+        # Tìm Default Gateway dựa trên IP đầu tiên
+        for iface in info["interfaces"]:
+            if iface["ipv4"]:
+                info["default_gateway"] = ".".join(iface["ipv4"].split(".")[:-1] + ["1"])
+                break
+
+        # DNS fallback
+        if os.name != 'nt':
+            dns_servers = []
             try:
                 with open("/etc/resolv.conf") as f:
                     for line in f:
@@ -104,9 +129,10 @@ def network_info():
                             dns_servers.append(line.strip().split()[1])
             except:
                 pass
+        else:
+            dns_servers = ["8.8.8.8", "8.8.4.4"]
 
-        if dns_servers:
-            info["DNS Servers"] = dns_servers
+        info["dns_servers"] = dns_servers
 
         return jsonify(info)
     except Exception as e:
